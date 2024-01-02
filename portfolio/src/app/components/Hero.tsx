e449264e4da5039ed4715b6c2449e8c1680198e6
Michael Di Giuseppe <mdigius@uwo.ca>
2024-01-02 16:48:07 -0500

    Added an about section, and the 3D asset for languages

--- portfolio/src/app/components/Hero.tsx
+++ portfolio/src/app/components/Hero.tsx
@@ -6,18 +6,42 @@ import Spline from '@splinetool/react-spline';
 
 const Hero = () => {
   return (
-    
-    <div className = "hero mt-20">
+    <main>
+      <div className="hero mt-20">
         <div className="flex-1 mt-5 padding-x">
-          <h1 className = "hero__title">Michael Di Giuseppe</h1>
-          <p className="hero__subtitle">Software Engineer</p>
+          <h1 className="hero__title">Michael Di Giuseppe</h1>
+          <p className="hero__subtitle">Software Engineering Student</p>
           <div>
-          <Spline scene="https://prod.spline.design/LvqbiIo9JA5QoUbK/scene.splinecode" className="-mt-10" />
+           <Spline scene="https://prod.spline.design/LvqbiIo9JA5QoUbK/scene.splinecode" className="-mt-10" />
           </div>
-          </div>
+        </div>
       </div>
 
-  )
+      <div className="hero">
+        <div className="flex-1 mt-5 padding-x">
+          <h1 className="hero__title">About Me</h1>
+
+
+          <h1 className="hero__title2">Projects</h1>
+          <p className="hero__subtitle">Quick look at my software projects. See more</p>
+
+          <h1 className="hero__title2">Programming Languages</h1>
+          <p className="hero__subtitle">Profiency in:</p>
+          <div>
+          <Spline scene="https://prod.spline.design/IECKvPJxVMNT1g2G/scene.splinecode" className="-mt-20"/>
+          </div>
+
+          <h1 className="hero__title2">Hobbies</h1>
+          <p className="hero__subtitle">Music</p>
+          <p className="hero__subtitle">AquaScaping</p>
+          <p className="hero__subtitle">Art</p>
+          
+        </div>
+        
+      </div>
+      
+    </main>
+  );
 }
 
 export default Hero
